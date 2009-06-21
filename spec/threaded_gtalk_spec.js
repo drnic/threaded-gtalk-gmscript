7491704f9e4cdc4ca7dd2aea2ce0685da6711b64
Dr Nic Williams <drnicwilliams@gmail.com>
2009-06-21 21:52:34 +1000

    added some spec samples

--- spec/threaded_gtalk_spec.js
+++ spec/threaded_gtalk_spec.js
@@ -2,5 +2,27 @@ require("spec_helper.js");
 require("../public/threaded_gtalk.js");
 
 Screw.Unit(function(){
+  describe("find threads", function(){
+    before(function(){
+      $('td.kw').remove(); // hide image menu links
+    });
+    it("#postman", function(){
+      
+    });
+    it("#banjo and #kids", function(){
+      
+    });
+  });
+  describe("live updates", function(){
+    before(function(){
+      // add a new message
+    });
+    it("should discover #parcel thread", function(){
+      
+    });
+    it("should attach #parcel thread to #postman thread", function(){
+      
+    });
+  });
 });
 
