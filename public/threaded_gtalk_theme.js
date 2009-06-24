b16e39658fa2f1532149af9833c519dad0d2235e
Dr Nic Williams <drnicwilliams@gmail.com>
2009-06-25 08:04:36 +1000

    reuse finite list of thread colours for any thread-N (N < 5*colour size)

--- public/threaded_gtalk_theme.js
+++ public/threaded_gtalk_theme.js
@@ -4,12 +4,18 @@ var ThreadedGtalk = ThreadedGtalk || {};
   ThreadedGtalk.View = (new function(){
     // Thread colours come from the colour sequence for participants in Gmail conversations
     this.threadColours = ['#006835', '#790619', '#5B1094', '#C88900', '#CC0060', '#00839F'];
+    this.repeatCount   = 5;
     
     this.installTheme = function() {
       var colourClasses = "";
-      $.each(this.threadColours, function(index) {
-        colourClasses += ".thread-" + (index + 1) + " { color: " + this + " }\n";
-      });
+      var colourLength  = this.threadColours.length;
+      var seq = 1;
+      for (var repeat = 0; repeat < this.repeatCount; repeat++) {
+        $.each(this.threadColours, function(index) {
+          colourClasses += ".thread-" + seq + " { color: " + this + " }\n";
+          seq++;
+        });
+      };
       $('head').append($('<style class="threaded_gtalk_theme">' + colourClasses + '</style>'));
     };
   });
