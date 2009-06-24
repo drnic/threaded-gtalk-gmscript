527e67f5a20d75ad8682b2c9835561f601bc86bd
Dr Nic Williams <drnicwilliams@gmail.com>
2009-06-25 06:32:19 +1000

    added required colours into spec titles

--- spec/install_threaded_gtalk_theme_spec.js
+++ spec/install_threaded_gtalk_theme_spec.js
@@ -13,10 +13,10 @@ Screw.Unit(function(){
       expect($('style.threaded_gtalk_theme').size()).to(equal, 1);
     });
     describe("#kids tag is 1st thread", function(){
-      it("should colour #kids message", function(){
+      it("should colour #kids message green", function(){
         expect($('.thread-1').css('color')).to(equal, 'rgb(0, 104, 53)');
       });
-      it("should colour #postman message", function(){
+      it("should colour #postman message maroon", function(){
         expect($('.thread-2').css('color')).to(equal, 'rgb(121, 6, 25)');
       });
     });
