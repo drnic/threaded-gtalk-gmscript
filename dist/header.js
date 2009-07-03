cca999bdf93f19f8f1d8e40e4d972bb7b1c84def
Dr Nic Williams <drnicwilliams@gmail.com>
2009-07-04 04:20:41 +1000

    updated gmscript

---
+++ dist/header.js
@@ -0,0 +1,8 @@
+// ==UserScript==
+// @name          Threaded Gtalk GMScript
+// @namespace     http://drnicwilliams.com/
+// @description   Instant autocompletion for any <select> drop downs on any page
+// @include       https://mail.google.com/*
+// @include       http://mail.google.com/*
+// @version       1.0
+// ==/UserScript==
