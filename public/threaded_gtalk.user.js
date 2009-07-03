51da2489c2c5f3281033ead3d5abbf2bb17d0a9a
Dr Nic Williams <drnicwilliams@gmail.com>
2009-07-04 03:21:04 +1000

    allow for google apps domains

--- public/threaded_gtalk.user.js
+++ public/threaded_gtalk.user.js
@@ -2,7 +2,8 @@
 // @name          Threaded Gtalk GMScript
 // @namespace     http://drnicwilliams.com/
 // @description   Instant autocompletion for any <select> drop downs on any page
-// @include       https://mail.google.com/mail/*
+// @include       https://mail.google.com/*
+// @include       http://mail.google.com/*
 // @version       1.0
 // ==/UserScript==
 
